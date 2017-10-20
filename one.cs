bf214247095fa7824b634c2c37760b6a5ce76ba9
邹晶晶 <1341640889@qq.com>
2017-10-20 18:44:42 +0800

    增加了一串代码

--- one.cs
+++ one.cs
@@ -28,3 +28,35 @@ namespace Mynamespace   //声明自己的名称空间，命名空间提供了一
     }
 
 }
+
+
+namespace P3_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            ComplexNumber c1; //定义结构变量c1
+            c1.a = 1.5;
+            c1.b = 3;
+            ComplexNumber c2 = c1; //定义结构变量c2
+            c2.a = c2.b;
+            Console.Write("c1 = ");
+            c1.Write();
+            Console.Write("c2 = ");
+            c2.Write();
+            Console.ReadLine();
+        }
+    }
+
+    struct ComplexNumber
+    {
+        public double a;
+        public double b;
+
+        public void Write()
+        {
+            Console.WriteLine("{0}+{1}i", a, b);
+        }
+    }
+}
